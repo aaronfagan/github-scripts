9341031db415396fa478ef60b28d63119a1fb5ed
Aaron Fagan <hello@aaronfagan.ca>
2021-10-28 16:41:29 -0600

    feat: update setting.yml

--- push-all.sh
+++ push-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 REPOS=$(ls)
-COMMIT_MSG='feat: update setting.yml'
+COMMIT_MSG="feat: update setting.yml"
 
 for REPO in ${REPOS}; do
     cd ./${REPO}
-    git add . && git commit -m ${COMMIT_MSG} && git push
+    git add . && git commit -m "${COMMIT_MSG}" && git push
     cd ~/Github
 done
