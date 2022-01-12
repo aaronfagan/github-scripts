7d090a5dfa73bf5a9b406ca9c0a23ac9f2ee2448
Aaron Fagan <hello@aaronfagan.ca>
2022-01-12 12:52:37 -0700

    feat: add change default branch script

--- clone-all.sh
+++ clone-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS=$(curl -s -X GET -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/repos?per_page=100 | jq -r .[].full_name)
+REPOS=$(curl -s -X GET -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/user/repos?per_page=100" | jq -r .[].full_name)
 
 for REPO in ${REPOS}; do
     git clone git@github.com:${REPO}.git
