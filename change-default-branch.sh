89f6d9179fbc481591ffb0c9153e4670c8a7f81d
Aaron Fagan <hello@aaronfagan.ca>
2022-01-12 12:37:10 -0700

    feat: add change script

--- change-default-branch.sh
+++ change-default-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-BRANCH_OLD="master"
-BRANCH_NEW="main"
+BRANCH_OLD="main"
+BRANCH_NEW="master"
 # REPOS=$(curl -s -X GET -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/repos?per_page=100 | jq -r .[].full_name)
 REPOS="aaronfagan/github-scripts"
 
@@ -11,7 +11,7 @@ for REPO in ${REPOS}; do
 	curl -s -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -d "{\"ref\": \"refs/heads/${BRANCH_NEW}\",\"sha\": \"${COMMIT_SHA}\"}" https://api.github.com/repos/${REPO}/git/refs
 	# Swap Default Branch to New One
 	echo "SWAP"
-	curl -s -X PATCH -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -d "{\"default_branch\": \"origin/${BRANCH_NEW}\" }"https://api.github.com/repos/${REPO}/git/refs
+	curl -s -X PATCH -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -d "{\"default_branch\": \"origin/${BRANCH_NEW}\" }" https://api.github.com/repos/${REPO}/git/refs
 	# Delete old Branch
 	echo "DELETE"
 	curl -s -X DELETE -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${REPO}/git/refs/heads/${BRANCH_OLD}
